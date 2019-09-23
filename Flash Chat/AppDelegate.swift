e19c04b76e5a81be71853e663b1278de3c6e2577
mcsaatchidevelopers <tools@mcsaatchi-milano.com>
2019-09-23 12:25:58 +0200

    up

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 
 @UIApplicationMain
@@ -15,10 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         //TODO: Initialise and Configure your Firebase here:
+        FirebaseApp.configure()
         
+        let myDatabase = Database.database().reference()
+        
+        myDatabase.setValue("I passed some data")
         
         return true
     }
--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -32,6 +32,18 @@ class RegisterViewController: UIViewController {
 
         
         //TODO: Set up a new user on our Firbase database
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (user, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Registration Success")
+                
+                // push to chat page programmatically
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
+        
         
         
 
